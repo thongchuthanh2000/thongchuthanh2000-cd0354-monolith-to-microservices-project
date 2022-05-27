7fc03891468a83d2ac3f2deb9c1c4729e75bd9bd
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2022-05-28 01:50:22 +0700

    Update

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aea1e9224755a49babea25a8b22a2fc0-322688954.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
